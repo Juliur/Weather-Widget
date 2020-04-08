5825dd8c405f6f2dc411dc5f5e094ea5b6304866
Julia <juliayrkina@gmail.com>
2020-04-08 13:08:02 +0300

    store rewrited

--- src/store.js
+++ src/store.js
@@ -5,35 +5,52 @@ import service from "./services/service";
 Vue.use(Vuex); // tell Vue to action with Vuex
 
 export default new Vuex.Store({
-    state: {
-        weather: {},
-        dataIsReceived: false,
-    },
+	state: {
+		weatherCache: null,
+	},
 
-    mutations: {
-      UPDATE_WEATHER(state){
-        service.getForecastData()
-        .then(response => {
-            if(response.statusText === "OK"){
-                // console.log(response.statusText)
-                state.weather = response.data;
-                state.dataIsReceived = true;
-            }
+	mutations:{
+		setWeatherCache(state, payload){
+			state.weatherCache = payload;
+		}
+	},
+
+	actions:{
+		async updateWeather({commit}){
+			let prevData = await service.getForecastData();
+			let finalData = prevData.data;
+			commit('setWeatherCache', finalData);
+		}
+	},
+
+	getters:{
+		getWeatherCache(state) {
+			return state.weatherCache
+		}
+	}
+    // mutations: {
+    //   UPDATE_WEATHER(state){
+    //     service.getForecastData()
+    //     .then(response => {
+    //         if(response.statusText === "OK"){
+    //             state.weather = response.data;
+    //             state.dataIsReceived = true;
+    //         }
             
-            // for(let i = 0; i < response.data.list.length; i+=8){
-            //     state.weather.dailyForecasts.push(response.data.list[i]);
-            // console.log(response)
+    //         // for(let i = 0; i < response.data.list.length; i+=8){
+    //         //     state.weather.dailyForecasts.push(response.data.list[i]);
+    //         // console.log(response)
            
-        })
-        .catch((err)=>{
-            console.log("There is an error", err);
-            state.dataIsReceived = false;
-        })
-    }
-    },
-    actions: {
-        updateWeather(context) {
-          context.commit('UPDATE_WEATHER');
-        }
-    }
+    //     })
+    //     .catch((err)=>{
+    //         console.log("There is an error", err);
+    //         state.dataIsReceived = false;
+    //     })
+    // }
+    // },
+    // actions: {
+    //     updateWeather(context) {
+    //       context.commit('UPDATE_WEATHER');
+    //     }
+    // }
 })
