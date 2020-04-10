c8134eef57eeb6fbd22787a0187e0d099acbb3f6
Julia <juliayrkina@gmail.com>
2020-04-10 15:22:24 +0300

    added getCitiesArray method

--- src/views/SettingsTab.vue
+++ src/views/SettingsTab.vue
@@ -6,15 +6,20 @@
 					<div class="col-12">
 						<input 
 							v-model="enteredCity"
+							@keyup.enter="getCitiesArray"
 							class="search-input"
 							type="text" 
 							placeholder="Enter your city..."/>
 						<button
 								type="submit"
-								@click.prevent="getWeather"
+								@click.prevent="getCitiesArray"
 								class="search-btn text-uppercase font-weight-bold">
 							<i class="fa fa-search" aria-hidden="true"></i>
 						</button>
+
+						<div class="city-variants" v-for="city in matchesCities" :key="city.id"> 
+							<p>{{city.name}}</p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -30,6 +35,7 @@ import axios from 'axios';
 		data(){
 			return{
 				enteredCity: "",
+				matchesCities: [],
 				cityCoordinates: {
 					lat: null,
 					lon: null,
@@ -37,35 +43,48 @@ import axios from 'axios';
 			}
 		},
 		methods:{
-			getWeather(){
-				axios.get("/city.list.min.json")
+			async getCitiesArray(){
+				try{
+					await axios.get("/city.list.min.json")
 					.then((response)=>{
-						for(let city of response.data){
-							if(city.name === this.enteredCity){
-								console.log(this.enteredCity)
-								let lat = city.coord.lat;
-								let lon = city.coord.lon;
-								this.cityCoordinates.lat = lat;
-								this.cityCoordinates.lon = lon;
-								break
-							}
+						let citiesArray = response.data;
+						this.matchesCities = citiesArray.filter(
+							city => city.name.toLowerCase().startsWith(this.enteredCity.toLowerCase())
+						);
+						console.log(this.matchesCities)
+						if(this.matchesCities.length === 0) {
+							throw new Error("Array is empty!")
 						}
-						return axios({
-							method: 'get',
-							url: "https://api.openweathermap.org/data/2.5/onecall",
-							params:{
-								lat: this.cityCoordinates.lat,
-								lon: this.cityCoordinates.lon,
-								units: "metric",
-							appid: "4442e1e742eb4c424c00335c7acb087e",
-							}
-						})
 					})
-					.then((response)=>{
-						console.log(response.data)
-					})
-					.catch(error => console.log("City not found! ", error.response.statusText));
-			},
-		},
+				} catch(error) {
+						console.log(error)
+					} 
+			}
+		}
 	}
+						// for(let city of response.data){
+						// 	if(city.name.toLowerCase() === this.enteredCity.toLowerCase()){
+						// 		console.log(this.enteredCity)
+						// 		let lat = city.coord.lat;
+						// 		let lon = city.coord.lon;
+						// 		this.cityCoordinates.lat = lat;
+						// 		this.cityCoordinates.lon = lon;
+						// 		break
+						// 	}
+						// }
+					// 	return axios({
+					// 		method: 'get',
+					// 		url: "https://api.openweathermap.org/data/2.5/onecall",
+					// 		params:{
+					// 			lat: this.cityCoordinates.lat,
+					// 			lon: this.cityCoordinates.lon,
+					// 			units: "metric",
+					// 		appid: "4442e1e742eb4c424c00335c7acb087e",
+					// 		}
+					// 	})
+					// })
+					// .then((response)=>{
+					// 	console.log(response.data)
+					// })
+					// .catch(error => console.log("City not found! ", error.response.statusText));
 </script>
