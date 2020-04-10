ae7ff46d8f5d70d4460c8c487345a6696614ece9
Julia <juliayrkina@gmail.com>
2020-04-10 12:41:51 +0300

    added getWeather  method for entered city

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 const API_KEY = "4442e1e742eb4c424c00335c7acb087e";
-// const API_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?id=703448&cnt=5&appid=${API_KEY}`;
 const API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=50.433334&lon=30.516666&units=metric&appid=${API_KEY}`
 export {API_URL};
 
--- src/views/SettingsTab.vue
+++ src/views/SettingsTab.vue
@@ -1,11 +1,70 @@
 <template>
-    <div id="settings">
-        <input type="text" placeholder="City">
-    </div>
+	<div id="weather-widget" class="container-fluid h-100 ">
+		<div class="row align-items-center h-100">
+			<div class="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2 col-12">
+				<div class="row no-gutters">
+					<div class="col-12">
+						<input 
+							v-model="enteredCity"
+							class="search-input"
+							type="text" 
+							placeholder="Enter your city..."/>
+						<button
+								type="submit"
+								@click.prevent="getWeather"
+								class="search-btn text-uppercase font-weight-bold">
+							<i class="fa fa-search" aria-hidden="true"></i>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>   
 </template>
 
 <script>
-    export default {
-        name: "SettingsTab",
-    }
+import axios from 'axios';
+
+	export default {
+		name: "SettingsTab",
+		data(){
+			return{
+				enteredCity: "",
+				cityCoordinates: {
+					lat: null,
+					lon: null,
+				}
+			}
+		},
+		methods:{
+			getWeather(){
+				axios.get("/city.list.min.json")
+					.then((response)=>{
+						for(let city of response.data){
+							if(city.name === this.enteredCity){
+								console.log(this.enteredCity)
+								let lat = city.coord.lat;
+								let lon = city.coord.lon;
+								this.cityCoordinates.lat = lat;
+								this.cityCoordinates.lon = lon;
+								break
+							}
+						}
+						return axios({
+							method: 'get',
+							url: "https://api.openweathermap.org/data/2.5/onecall",
+							params:{
+								lat: this.cityCoordinates.lat,
+								lon: this.cityCoordinates.lon,
+								units: "metric",
+							appid: "4442e1e742eb4c424c00335c7acb087e",
+							}
+						})
+					})
+					.then((response)=>{
+						console.log(response.data)
+					})
+			},
+		},
+	}
 </script>
